d1ff3e6aeece87635eb8e3ed4fd8461ac6ae12a8
b1-ing <brendan.ling@student.manchester.ac.uk>
2025-02-10 17:02:22 +0800

    halfway with geolocation

--- quietcorner/src/app/main/page.tsx
+++ quietcorner/src/app/main/page.tsx
@@ -5,6 +5,7 @@ import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import 'ol/ol.css';
+import Geolocation from 'ol/Geolocation.js';
 import Feature from 'ol/Feature';
 import Point from 'ol/geom/Point';
 import { Vector as VectorSource } from 'ol/source';
@@ -63,6 +64,65 @@ function MapLayout() {
     }, []);
 
     useEffect(() => {
+        const view = new View({
+            center: [0, 0],
+            zoom: 2,
+        });
+
+        const geolocation = new Geolocation({
+            // enableHighAccuracy must be set to true to have the heading value.
+            trackingOptions: {
+                enableHighAccuracy: true,
+            },
+            projection: view.getProjection(),
+        });
+
+        function el(id) {
+            return document.getElementById(id);
+        }
+
+        el('track').addEventListener('change', function () {
+            geolocation.setTracking(this.checked);
+        });
+
+// update the HTML page when the position changes.
+        geolocation.on('change', function () {
+            el('accuracy').innerText = geolocation.getAccuracy() + ' [m]';
+            el('altitude').innerText = geolocation.getAltitude() + ' [m]';
+            el('altitudeAccuracy').innerText = geolocation.getAltitudeAccuracy() + ' [m]';
+            el('heading').innerText = geolocation.getHeading() + ' [rad]';
+            el('speed').innerText = geolocation.getSpeed() + ' [m/s]';
+        });
+
+
+        const accuracyFeature = new Feature();
+        geolocation.on('change:accuracyGeometry', function () {
+            accuracyFeature.setGeometry(geolocation.getAccuracyGeometry());
+        });
+
+
+        const positionFeature = new Feature();
+        positionFeature.setStyle(
+            new Style({
+                image: new Circle({
+                    radius: 6,
+                    fill: new Fill({
+                        color: '#3399CC',
+                    }),
+                    stroke: new Stroke({
+                        color: '#fff',
+                        width: 2,
+                    }),
+                }),
+            }),
+        );
+
+        geolocation.on('change:position', function () {
+            const coordinates = geolocation.getPosition();
+            positionFeature.setGeometry(coordinates ? new Point(coordinates) : null);
+        });
+
+
         if (!map) {
             const initialMap = new Map({
                 target: mapElement.current!,
@@ -80,6 +140,7 @@ function MapLayout() {
                 }),
             });
 
+
             setMap(initialMap);
         }
     }, []);
@@ -88,6 +149,7 @@ function MapLayout() {
         if (!map) return;
 
         vectorSource.clear(); // Remove old markers
+
 
         locations.forEach((location) => {
             const feature = new Feature({
